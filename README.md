baeae2021d0bc782add573d188c62af76b4f4cde
Steven Ng <stevenng308@gmail.com>
2013-11-24 21:36:09 -0500

    Beginning initial setup of project. Included the necessary files.

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ OS-3243-Project-6
 =================
 
 Contributors: Steven Ng and Jestin Keaton
+Due: Dec 2 2013
---
+++ partone.cpp
@@ -0,0 +1,6 @@
+// CS3243 Operating Systems
+// Fall 2013
+// Project 6: Disks and File Systems
+// Jestin Keaton and Steven Ng
+// Date: 12/2/2013
+// File: partone.cpp
