eb495a7a3cca7eb36f4e9a10542a29ebf11ab2be
Steven Ng <stevenng308@gmail.com>
2013-11-26 20:03:14 -0500

    Added method to load the floppy image files into memory. Started a method to print memory map.

--- partOneTest.cpp
+++ partOneTest.cpp
@@ -9,6 +9,7 @@
 #include<iostream>
 #include<fstream>
 #define BYTECOUNT 1474560
+#define FIRST_BYTE_ENTRY 16896
 
 using namespace std;
 
@@ -16,16 +17,24 @@ using namespace std;
 
 typedef unsigned char byte;
 
+string bar = "           |----+----|----+----|----+----|----+----|----+----|----+----|----+----|----+----\n";
+
 struct MainMemory{
     byte memArray[BYTECOUNT]; // 0-511 is for the boot partition
+    
+    MainMemory();
     int findFreeMemory();
+    void insertIntoMemory(byte b);
+    void print();
 };
 
+MainMemory memory;
 
-
+void loadSystem();
 int main(){
-    ifstream ifile("fdd.flp",std::ifstream::in);
+    /*ifstream ifile("fdd.flp",std::ifstream::in);
     byte b = ifile.get();
+
 
     //byte copiedFile
 
@@ -43,9 +52,76 @@ int main(){
         else
             ++c;
     }
+    printf("%d", memory.findFreeMemory());*/
+    loadSystem();
+    memory.print();
+    
     return 0;
 }
 
+void loadSystem()
+{
+	ifstream ifile("fdd.flp",std::ifstream::in);
+    byte b = ifile.get();
+
+    //byte copiedFile
+
+    int c = 0;
+    while (ifile.good()){
+		if(b
+		memory.memArray[c] = b; 
+		b = ifile.get();
+		++c;
+	}
+}
+
+MainMemory::MainMemory()
+{
+	for (int i = 0; i < BYTECOUNT; i++)
+	{
+		memArray[i] = 157;
+	}
+}
+
 int MainMemory::findFreeMemory(){
-    return 0;
+    for (int i = 0; i < BYTECOUNT; i++)
+	{
+		if (memArray[i] == 157)
+		{
+			return i;
+		}
+	}
+	return -1;
+}
+
+void MainMemory::insertIntoMemory(byte b)
+{
+	memory.memArray[0] = b;
+}
+
+void MainMemory::print()
+{
+	cout << "\nDISK USAGE BY SECTOR:\n";
+	cout << bar;
+	for(int i = 0; i < 36; i++){
+		int begin = (i*80);
+		int end = (i+1)*80-1;
+		printf("%04d-%04d: ",begin,end);
+		char toPrint;
+		for(int j = begin; j <= end; j++){
+			toPrint = '.';
+			if(j==0){
+				toPrint = 'B';
+			}
+			else if(j < 19){
+				toPrint = 'F';
+			}
+			else if(j < 33){
+				toPrint = 'R';
+			}
+			printf("%c",toPrint);
+		}
+		cout << endl;
+	}
+	cout << endl;
 }
