cb352a0d8a1981ce19e334de0dac7dae215152cf
Jestin Keaton <jestinlk@gmail.com>
2013-11-28 03:51:31 -0500

    added methods to set and get FAT table entries

--- partOneTest.cpp
+++ partOneTest.cpp
@@ -15,6 +15,8 @@
 #define SECTOR_SIZE 512
 #define MAX_NUM_ENTRY 2849 //2880 - 33 = 2847 + 2 reserved = 2849
 #define START_FAT 2
+#define FIRST_FAT_BYTE 512
+#define FAT_SIZE 4608
 
 using namespace std;
 
@@ -33,6 +35,9 @@ struct MainMemory{
     void print();
 };
 
+
+// Since we can access the FAT directly, there may be no need for these extra structs
+/**
 struct Entry
 {
 	char a : 8;
@@ -43,12 +48,19 @@ struct Entry
 struct FileTable
 {
 	Entry table[MAX_NUM_ENTRY];
-};
+};*/
+
+
 
 MainMemory memory;
 //deque<int> freeSectors;
 
+
+// Declare Methods
 void loadSystem();
+void setEntry(short a);
+short getEntry(short b);
+
 int main(){
     /*ifstream ifile("fdd.flp",std::ifstream::in);
     byte b = ifile.get();
@@ -73,10 +85,10 @@ int main(){
     printf("%d", memory.findFreeMemory());*/
     loadSystem();
     memory.print();
-    Entry e = {0, 11, 63};
-    printf("%d\n", e.b);
-    printf("%d\n", e.c);
-    printf("%d\n", (e.b << 8) + e.c);
+    //Entry e = {0, 11, 63};
+    //printf("%d\n", e.b);
+    //printf("%d\n", e.c);
+    //printf("%d\n", (e.b << 8) + e.c);
     /*memory.findFreeSector();
 	for (uint i = 0; i < freeSectors.size(); i++)
 	{
@@ -99,6 +111,48 @@ void loadSystem()
 		b = ifile.get();
 		++c;
 	}
+}
+
+/**
+* Sets a FAT and FAT2 entries to be the short value that is entered
+* parama a the value we intend to set in the FAT
+*/
+void setEntry(short a){
+    // In the schema [yz Zx XY], the lower-case letters represent what we call
+    // the low-order entry, the high-order entry would be the upper-case letters.
+    if(a<0 || a >= MAX_NUM_ENTRY)
+        return; // faulty sector selection, do nothing
+    // Set both FAT and FAT2 entries 
+    for(int i = 0; i < 2; i++){
+        int start = (FIRST_FAT_BYTE + i * FAT_SIZE) + (a/2) * 3; // set first byte of entry pair
+        if(a%2==0){ // setting a low-order entry
+            memory.memArray[start] = (a &  0xFF); // set yz to the lowest byte of short <a>
+            memory.memArray[start + 1] &= 0xF0; // clear low nibble here
+            memory.memArray[start + 1] |= ((a >> 8) & 0x0F); // set x to nibble 2 of <a>
+        }
+        else{ // setting a high-order entry
+            memory.memArray[start + 2] = ((a & 0x0FF0) >> 4); // set XY to nibbles 2 and 3 of <a>
+            memory.memArray[start + 1] &= 0x0F; // clear high nibble here
+            memory.memArray[start + 1] |= ((a & 0xF) << 4); // set Z to nibble 4 in <a>
+        }
+    }
+}
+
+/**
+* Retrieves the value sent in the parameter from the FAT table
+* param b the index in the FAT table that we want
+*/
+short getEntry(short b){
+    // Using the same schema as in 'setEntry()' we will extract a FAT entry's
+    // value and return it as a short [schema: yz Zx XY]
+    if(b<0 || b >= MAX_NUM_ENTRY)
+        return -1; // faulty request here, return error code (-1)
+    if(b%2==0){ // requesting a low-order entry
+        return memory.memArray[b/2] + ((memory.memArray[b/2 + 1] & 0x0F) << 8);
+    }
+    else{ // requesting a high-order entry
+        return (memory.memArray[b/2 + 2] << 4) + (memory.memArray[b/2 + 1] >> 4);
+    }
 }
 
 MainMemory::MainMemory()
