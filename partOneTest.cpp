140bc94969003105e64e4ba4c2d60d0c878db88f
Jestin Keaton <jestinlk@gmail.com>
2013-12-02 00:36:40 -0500

    fixed ofstream function to write to binary file

--- partOneTest.cpp
+++ partOneTest.cpp
@@ -84,7 +84,7 @@ int getUsedBytes();
 short getUsedSectors();
 short *filesAndSectorStats();
 void updateAccessDate(int startByte);
-void writeBackupFloppy(string s);
+void writeBackupFloppy();
 
 // Requested User Options
 void listDirectory();   // option # 1
@@ -164,7 +164,7 @@ int main(){
                 sectorDump();
                 break;
             case 10:
-                writeBackupFloppy("current.bin");
+                writeBackupFloppy();
                 break;
             default:
                 return 0;
@@ -196,11 +196,13 @@ void loadSystem()
 * Then it calls dd from within the program to convert the binary file to flp format. 
 * param s the name of the new file to be created, the flp image will have the same filename.
 */
-void writeBackupFloppy(string s){
+void writeBackupFloppy(){
+    string s = "current.bin";
     ofstream outfile(s.c_str(), ofstream::binary/*ofstream::out | ios::binary*/);
     byte* buffer = new byte[BYTECOUNT];
     for(int i = 0; i < BYTECOUNT; i++)
         buffer[i] = memory.memArray[i]; // copy the byte from simulated disk to binary file
+    outfile.write ((char*)buffer,BYTECOUNT);
     delete[] buffer;
     outfile.close();
     string res = "dd status=noxfer conv=notrunc if=";
